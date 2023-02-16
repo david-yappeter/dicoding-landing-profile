289270c4a90a68b2e83a96a4ba4dd63ff985abe6
David Yappeter <davidyap11les@gmail.com>
2023-02-16 23:41:30 +0700

    feat: add some basic styling

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,8 +5,20 @@ module.exports = {
     extend: {
       colors: {
         custom: {
-          primary: 'rgb(0,0,0)',
+          primary: 'rgb(19, 0, 90)',
+          'primary-light': 'rgb(0, 51, 124)',
+          tosca: 'rgb(28, 130, 173)',
+          green: 'rgb(3, 201, 136)',
         },
+      },
+      fontSize: {
+        sm: '0.8rem',
+        base: '1rem',
+        xl: '1.25rem',
+        '2xl': '1.563rem',
+        '3xl': '1.953rem',
+        '4xl': '2.441rem',
+        '5xl': '2.7rem',
       },
       fontFamily: {
         josefin: [
