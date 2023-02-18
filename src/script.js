cfd2a063068fd654a4ce89900301ad7e36c834dd
David Yappeter <davidyap11les@gmail.com>
2023-02-18 10:48:10 +0700

    feat:  add more content

--- src/script.js
+++ src/script.js
@@ -1,9 +1,12 @@
 document.addEventListener('DOMContentLoaded', function () {
-  document.addEventListener('scroll', function () {
-    if (window.scrollY > 0) {
-      document.querySelector('#header').classList.remove('at-top');
-    } else {
-      document.querySelector('#header').classList.add('at-top');
-    }
-  });
+  onScroll();
+  document.addEventListener('scroll', onScroll);
 });
+
+function onScroll() {
+  if (window.scrollY > 0) {
+    document.querySelector('#header').classList.remove('at-top');
+  } else {
+    document.querySelector('#header').classList.add('at-top');
+  }
+}
